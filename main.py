9bae77e36f9947f0166c39f1ba0b66aaeec48c18
Rene jalvin <bjalvin@bancolombia.com>
2023-04-16 16:42:14 -0500

    sin problemas

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from typing import List
 
 
 def show_payments(payments: List[int]) -> str:
-    # este es un comentario muy lagor y necesito formatearlos a uno mas corot en dos lienas se ben serparar para que se mas facil de leeer
+    # este es un comentario muy lagor y necesito formatearlos
+    # a uno mas corot en dos lienas
+    # se ben serparar para que se mas facil de leeer esto es un problemas
     result_payments: str = ""
     for payment in payments:
         if len(result_payments) == 0:
@@ -13,3 +15,14 @@ def show_payments(payments: List[int]) -> str:
 
 
 print(show_payments([100000, 200000, 400000, "100000"]))
+
+# este codigo a continuacion no es recomendado se hace con el fin de
+# de que se lance el erro de bandit hook
+
+# import hashlib
+
+# password = "secreto123"
+# salt = "abcd1234"
+
+# hash = hashlib.md5(password + salt).hexdigest()
+# print("Hash: ", hash)
